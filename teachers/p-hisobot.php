d2e9d5894e34393bfc76c96355d1226733221b9d
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2026-02-20 21:21:37 +0500

    Update and rename p-hisobot.php to p-hisobot.php

--- teachers/p-hisobot.php
+++ teachers/p-hisobot.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../inc/db.php';
-require_once __DIR__ . '/../inc/auth.php';
+require_once __DIR__ . '/../inc/tauth.php';
 
 session_start_secure();
 require_role('viewer');
